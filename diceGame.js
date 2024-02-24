6e60a61c60cdad884ae4c9b481dc6ce2bcbbb61f
kotteshtechboss07 <kottesh2005@gmail.com>
2024-02-24 17:07:51 +0530

    dice

--- diceGame.js
+++ diceGame.js
@@ -50,8 +50,8 @@ const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
-  // player0El.classList.toggle('player--active');
-  // player1El.classList.toggle('player--active');
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
 };
 
 // Rolling dice functionality
