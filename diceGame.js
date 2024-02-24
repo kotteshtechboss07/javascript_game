66ee0d59408fcae74a71bac452ef9aea5cbe4ff3
kotteshtechboss07 <kottesh2005@gmail.com>
2024-02-24 20:37:55 +0530

    dice_game2changes

--- diceGame.js
+++ diceGame.js
@@ -1,14 +1,13 @@
-// /*
-// GAME RULES:
-// - The game has 2 players, playing in rounds, set the final score
-// - In each turn, a player rolls the tow dices as many times as he whishes. Each result get added to his ROUND score
-// - BUT, if the player rolls a 1 among one of the dices, all his ROUND score gets lost. After that, it's the next player's turn
-// - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-// - The first player to reach the final score points on GLOBAL score wins the game
-// /*
-// Game Two: A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
-// */ 
-
+// // /*
+// // GAME RULES:
+// // - The game has 2 players, playing in rounds, set the final score
+// // - In each turn, a player rolls the tow dices as many times as he whishes. Each result get added to his ROUND score
+// // - BUT, if the player rolls a 1 among one of the dices, all his ROUND score gets lost. After that, it's the next player's turn
+// // - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+// // - The first player to reach the final score points on GLOBAL score wins the game
+// // /*
+// // Game Two: A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
+ // */ 
 'use strict';
 
 // Selecting elements
@@ -33,10 +32,10 @@ const init = function () {
   activePlayer = 0;
   playing = true;
 
-  // score0El.textContent = 0;
-  // score1El.textContent = 0;
-  // current0El.textContent = 0;
-  // current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
 
   diceEl.classList.add('hidden');
   player0El.classList.remove('player--winner');
@@ -63,13 +62,14 @@ btnRoll.addEventListener('click', function () {
     // 2. Display dice
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
-    // diceEl.alt = `dice-${dice}`;
 
     // 3. Check for rolled 1
     if (dice !== 1) {
       // Add dice to current score
       currentScore += dice;
-      document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+      document.getElementById(
+        `current--${activePlayer}`
+      ).textContent = currentScore;
     } else {
       // Switch to next player
       switchPlayer();
@@ -83,7 +83,8 @@ btnHold.addEventListener('click', function () {
     scores[activePlayer] += currentScore;
     // scores[1] = scores[1] + currentScore
 
-    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
 
     // 2. Check if player's score is >= 100
     if (scores[activePlayer] >= 100) {
@@ -91,8 +92,12 @@ btnHold.addEventListener('click', function () {
       playing = false;
       diceEl.classList.add('hidden');
 
-      document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
-      document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
     } else {
       // Switch to the next player
       switchPlayer();
