255f8bc322927469e7dad76b71e3fd52a49f5fff
kotteshtechboss07 <kottesh2005@gmail.com>
2024-03-11 15:44:08 +0530

    paramapadham_game

---
+++ paramapadham.js
@@ -0,0 +1,6 @@
+'use strict'
+const player0 =document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
+const score0 = document.querySelector('score--0');
+const score1 = document.getElementById('score--1');
+
